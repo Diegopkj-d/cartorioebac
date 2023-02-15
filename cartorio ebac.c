f6c757a456a24f2f3ba1de3640b4f41c3dbe03da
Diegopkj-d <125473084+Diegopkj-d@users.noreply.github.com>
2023-02-15 11:18:22 -0300

    add function 4 to the menu for exit program

--- cartorio ebac.c
+++ cartorio ebac.c
@@ -118,6 +118,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
     	printf("\t3 - Deletar nomes\n");
+    	printf("\t4 - Sair\n");
     	printf("\t Opção: "); //fim do menu
     	scanf("%d", &opcao); //armazenando a escolha do usuário
     	system("cls");
@@ -134,6 +135,11 @@ int main()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o nosso sistema!\n\n");
+		return 0;
 		break;
 		
 		default: 
